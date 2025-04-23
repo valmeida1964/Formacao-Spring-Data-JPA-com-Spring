b4cde6dd2d9870b169b2e05a898a02f6e3933fb7
vagner.almeida <valmeida1964@hotmail.com>
2025-04-23 13:18:52 -0300

    projeto criado no spring boot initializr

---
+++ produtosapi/produtosapi/src/test/java/com/vagner/produtosapi/ProdutosapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vagner.produtosapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdutosapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
